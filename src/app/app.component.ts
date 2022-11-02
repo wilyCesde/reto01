b796f19a6344759f4d8e4ec18a230d0672aaf6ef
Wilian Ruiz Zapata <wruizza@cesde.net>
2022-11-02 14:02:52 -0500

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  cities = ['Barcelona', 'Madrid','Lima']
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
+  cities = ['Barcelona', 'Madrid', 'Lima']
   name!: string;
+  selection!: string;
   title = 'reto01';
   url = 'https://www.xtrafondos.com/wallpapers/resized/edificios-ciudad-de-noche-3183.jpg?s=large'
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Change_>', changes);
+  }
+  //para optener la data o llamar api
+  ngOnInit(): void {
+    console.log('onInit->')
+  }
+  //observabls
+  ngOnDestroy(): void {
+    console.log('Destroy->');
+  }
+  onClicked(city: string): void {
+    console.log('City ->', city);
+    this.selection = city;
+  }
+
+
+
 
 
 }
